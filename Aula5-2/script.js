854667bc455769ebf8445782eaf0c46eea6e8fef
JPingarilho <160868745+JPingarilho@users.noreply.github.com>
2024-03-25 11:09:00 -0300

    Add files via upload

---
+++ Aula5-2/script.js
@@ -0,0 +1,17 @@
+console.log('começou');
+
+let lista = [1,2,3,4];
+
+function minhaFunc(meuPar){
+    return 'não faz ainda' + meuPar;
+}
+
+console.log('no meio');
+
+lista.push('novo');
+
+console.log(lista.length);
+
+console.log(minhaFunc(' ou tudo. '));
+
+console.log('terminou');
